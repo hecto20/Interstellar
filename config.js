1d75e7129dc556f4841db4bdb094fea0adbe88bf
hecto20 <127668390+hecto20@users.noreply.github.com>
2024-02-16 12:27:02 +0100

    Añadidas claves de acceso

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 const config = {
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
-  challenge: false,
+  challenge: true,
   users: {
     // username: 'password', you can add multiple users.
-    interstellar: 'password',
+    TorresGemelasAdmin: 'torresgemelas2024',  
+    Hector: 'Hector2010.holiwi/',
+    Alberto: 'AlbertoEspejoContraseña',
   },
 }
 export default config
